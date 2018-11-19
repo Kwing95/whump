d79dd6f0a5639f62c5c8152e459222e6cbdacc69
githabibi <hashemh@umich.edu>
2018-11-19 18:45:47 -0500

    Added routing

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Home     from "./components/Home.js";
+import About    from "./components/About.js";
+import Choice   from "./components/Choice.js";
+import Location from "./components/Location.js";
+import Schedule from "./components/Schedule.js";
+import Game     from "./components/Game.js";
+
 class App extends Component{
    render(){
       return(
-         <div>
-            <h1>Hello World</h1>
-         </div>
+        <BrowserRouter>
+          <div>
+            <Route path="/"         component={Home} exact/>
+            <Route path="/About"    component={About}/>
+            <Route path="/Choice"   component={Choice}/>
+            <Route path="/Location" component={Location}/>
+            <Route path="/Schedule" component={Schedule}/>
+            <Route path="/Game"     component={Game}/>
+
+          </div>
+        </BrowserRouter>
       );
    }
 }
