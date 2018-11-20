3fd6efb12824e521435c785ba414b0ea9b87c4e8
githabibi <hashemh@umich.edu>
2018-11-19 21:19:23 -0500

    Added basic skeleton for Schedule

--- prototype/src/components/Schedule.js
+++ prototype/src/components/Schedule.js
@@ -4,6 +4,22 @@ const Schedule = () => {
   return(
     <div>
       <h1>Schedule</h1>
+      <div class="row">
+        <div class="col">
+          <p>Subj/Num</p>
+        </div>
+        <div class="col">
+          <p>Location</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <input type="text"/>
+        </div>
+        <div class="col">
+          // Insert Wesley's code here
+        </div>
+      </div>
     </div>
   )
 }
