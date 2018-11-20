f34f5a293c5a4d35227919c6b4325d083c98d7c9
githabibi <hashemh@umich.edu>
2018-11-19 20:53:43 -0500

    Added import for about text along with button functionality

--- prototype/src/components/About.js
+++ prototype/src/components/About.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
+import { aboutText } from './variables.js';
 
 const About = () => {
   return(
     <div>
-      <h1>About</h1>
+      <h1>{ aboutText }</h1>
+      <Link to='/Choice'>
+        <button>I&apos;m ready!</button>
+      </Link>
     </div>
   )
 }
