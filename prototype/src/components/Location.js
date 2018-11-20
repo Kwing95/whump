992f14914ad54e2a643678c42e651ae68176466f
Wesley Pederson <pedersonw2015@gmail.com>
2018-11-20 17:12:53 -0500

    Changed title

--- prototype/src/components/Location.js
+++ prototype/src/components/Location.js
@@ -5,7 +5,7 @@ import { locations } from "./variables.js";
 const Location = () => {
   return(
     <div>
-      <h1>Location</h1>
+      <h1>Select Location and Destination</h1>
       <p>Start Location</p>
       <select name="start_loc">
         { 
