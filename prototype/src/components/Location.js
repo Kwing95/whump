74ab1df995c649dcad5aeb2831cea1f820e69dbc
Wesley Pederson <pedersonw2015@gmail.com>
2018-11-19 21:12:34 -0500

    rough version of Location.js made

--- prototype/src/components/Location.js
+++ prototype/src/components/Location.js
@@ -4,6 +4,15 @@ const Location = () => {
   return(
     <div>
       <h1>Location</h1>
+      <p>Start Location</p>
+      <select name="start_loc">
+        {this.locations.map((loc) => "<option value=\""+loc+"\" >"+loc+"</option>")} 
+      </select>
+      <p>End Location</p>
+      <select name="end_loc">
+        {this.locations.map((loc) => "<option value=\""+loc+"\" >"+loc+"</option>")} 
+      </select>
+      <button>Submit</button>
     </div>
   )
 }
