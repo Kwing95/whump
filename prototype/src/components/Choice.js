5885952e63cca84b7b63078a390f89d7e467edb8
githabibi <hashemh@umich.edu>
2018-11-20 13:17:57 -0500

    Reworked structure to work well with CSS

--- prototype/src/components/Choice.js
+++ prototype/src/components/Choice.js
@@ -2,17 +2,21 @@ import React from "react";
 import { Link } from 'react-router-dom';
 const Choice = () => {
   return(
-    <div>
-      <h1>Choice</h1>
-      <Link to="/Location">
-        <button>Input Locations</button>
-      </Link>
-      <Link to="/Schedule">
-        <button>Input Schedule</button>
-      </Link>
-      <Link to="/Game">
-        <button>Select Random</button>
-      </Link>
+    <div class="home-container">
+      <div class="center-horizontal">
+        <h1 class="text">Input Choice</h1>
+        <div class="choices-container">
+          <Link to="/Location">
+            <button class="btn-choice text">Input start and end location</button>
+          </Link>
+          <Link to="/Schedule">
+            <button class="btn-choice text">Input your schedule</button>
+          </Link>
+          <Link to="/Game">
+            <button class="btn-choice text">Play with random Locations!</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
