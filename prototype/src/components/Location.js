724cc88ae8d52cee2ab89608d72e9da2d5295eec
githabibi <hashemh@umich.edu>
2018-11-19 21:29:32 -0500

    Fixed location to display drop down properly

--- prototype/src/components/Location.js
+++ prototype/src/components/Location.js
@@ -8,11 +8,25 @@ const Location = () => {
       <h1>Location</h1>
       <p>Start Location</p>
       <select name="start_loc">
-        { locations.map((loc) => "<option value=\""+loc+"\" >"+loc+"</option>/n") } 
+        { 
+          locations.map((loc) => { 
+            return(
+              <option value="{ loc }" >{ loc } </option>
+            ) 
+            }
+          )
+        } 
       </select>
       <p>End Location</p>
       <select name="end_loc">
-        { locations.map((loc) => "<option value=\""+loc+"\" >"+loc+"</option>/n") } 
+        { 
+          locations.map((loc) => { 
+            return(
+              <option value="{ loc }" >{ loc } </option>
+            ) 
+            }
+          )
+        } 
       </select>
       <button>Submit</button>
     </div>
