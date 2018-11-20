a922a1aa3e3eef4f8af13200f8b5e41524f3a4b9
githabibi <hashemh@umich.edu>
2018-11-19 21:01:49 -0500

    Added a variable to hold supported locations

--- prototype/src/components/variables.js
+++ prototype/src/components/variables.js
@@ -4,4 +4,6 @@ const gameName = "Game Name";
 
 const aboutText = "This game is intended to help teach you how to get to your classes using the Ann Arbor busses quickly so you never have to miss class!";
 
-export { gameName, aboutText };
+const locations = ['BBB', 'Dude', 'Lorch'];
+
+export { gameName, aboutText , locations};
