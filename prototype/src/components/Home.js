0458be0d0d27b10d28fb4636680d45e955008a54
githabibi <hashemh@umich.edu>
2018-11-19 20:53:51 -0500

    Fixed import for game name

--- prototype/src/components/Home.js
+++ prototype/src/components/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-import gameName from './variables.js';
+import { gameName } from './variables.js';
 
 const Home = () => {
   return(
     <div>
       <h1>Welcome to { gameName }!</h1>
-      <Link to="/About"><button>Get Started!</button></Link>
+      <Link to="/About">
+        <button>Get Started!</button>
+      </Link>
     </div>
   )
 }
