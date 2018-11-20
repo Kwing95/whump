817cd36e41f7a94329cc669402f5475a7fa32e96
githabibi <hashemh@umich.edu>
2018-11-20 10:52:06 -0500

    Added an extra class for control over text

--- prototype/src/components/Home.js
+++ prototype/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return(
     <div class="home-container">
       <div class="home info">
-        <h1>Welcome to { gameName }!</h1>
+        <h1 class="home-text">Welcome to { gameName }!</h1>
         <Link to="/About">
           <button class="homeButton">Get Started!</button>
         </Link>
