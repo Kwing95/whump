5c03625161432ac0768addc6f5cf73c89d3ea8d1
githabibi <hashemh@umich.edu>
2018-11-20 16:40:35 -0500

    Reorganized the code

--- prototype/src/components/Game.js
+++ prototype/src/components/Game.js
@@ -1,48 +1,23 @@
-
 import React, {Component} from 'react'
 
-export default class Game extends Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    return(
-      <div>
-        <div class="wrapper">
-          <nav id="sidebar">
-            <div class="sidebar-header">
-              <h3>Clock</h3>
-            </div>
-            <div class="sidebar-header">
-              <h3>Class Schedule</h3>
-            </div>
-            <div class="sidebar-header">
-              <h3>Buses</h3>
-            </div>
-          </nav>
-        </div>
-      </div>
-    )
-  }
-
-}
-
-/*
 const Game = () => {
   return(
     <div>
-      <h1>Game</h1>
-      <Sidebar side='left' isVisible={ this.state.isVisible } onHide={ () => this.updateModal(false) }>
-                    <Nav>
-                      <NavItem href="#">Link 1</NavItem>
-                      <NavItem href="#">Link 2</NavItem>
-                      <NavItem href="#">Link 3</NavItem>
-                      <NavItem href="#">Link 4</NavItem>
-                    </Nav>
-                  </Sidebar>
+      <div class="wrapper">
+        <nav class="sidebar">
+          <div class="sidebar-header">
+            <h3>Clock</h3>
+          </div>
+          <div class="sidebar-header">
+            <h3>Class Schedule</h3>
+          </div>
+          <div class="sidebar-header">
+            <h3>Buses</h3>
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
 
-export default Game;*/
+export default Game;
