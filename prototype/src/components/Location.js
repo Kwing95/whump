e03fc87ba165a784d657b0ea66e3f8a8176981ed
Wesley Pederson <pedersonw2015@gmail.com>
2018-11-20 23:20:23 -0500

    fixed a thing

--- prototype/src/components/Location.js
+++ prototype/src/components/Location.js
@@ -15,7 +15,7 @@ const Location = () => {
           <div class="row">
             <div class="col-md-6 col-sm-6 col-xs-6">
               <h3 class="text">Start Location</h3>
-              <select name="start_loc" onChange={ (event) => {start_submission = event.target.value; start_submission = mapper[start_submission];} }>
+              <select name="start_loc" onChange={ (event) => {var temp = event.target.value; start_submission = mapper[temp];} }>
                 { 
                   locations.map((loc) => { 
                     return(
@@ -27,7 +27,7 @@ const Location = () => {
             </div>
             <div class="col-md-6 col-sm-6 col-xs-6">
               <h3 class="text">End Location</h3>
-              <select name="end_loc" onChange={ (event) => {end_submission = event.target.value; end_submission = mapper[end_submission]; }}>
+              <select name="end_loc" onChange={ (event) => {var temp = event.target.value; end_submission = mapper[temp];} }>
                 { 
                   locations.map((loc) => { 
                     return(
